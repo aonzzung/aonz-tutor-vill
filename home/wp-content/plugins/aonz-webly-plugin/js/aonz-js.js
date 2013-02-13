a09750920ee41baeebd52b6f7c42a41647742147
aonzzung <bugaonz@gmail.com>
2013-02-13 18:31:14 +0700

    Added student register modal

--- home/wp-content/plugins/aonz-webly-plugin/js/aonz-js.js
+++ home/wp-content/plugins/aonz-webly-plugin/js/aonz-js.js
@@ -1,36 +1,127 @@
 var jq = jQuery;
 
 jq(document).ready(function() {
-	
+
 	// register for register button
 	jq("[href='#tutor_register']").click(function() {
 
-//		jq.modal("<div><h1>Aonz SimpleModal</h1></div>");
-		
-		jq('#tutor_register_modal').modal(
-		{
-			overlayId: 'aonz-simplemodal-login-overlay',
-			containerId: 'aonz-simplemodal-login-container',
-			opacity:85,
-			//onShow: SimpleModalLogin.show,
-			position: ['15%', null],
-			zIndex:10000
+		jq('#tutor_register_modal').modal({
+			overlayId : 'aonz-simplemodal-overlay',
+			containerId : 'aonz-simplemodal-container',
+			opacity : 85,
+			// onShow: SimpleModalLogin.show,
+			position : [ '15%', null ],
+			zIndex : 10000
 		});
 	});
-	
-	/* Comment form submit */
-	jq("#commentform input#submit").click(function() {
 
-		var content = jq("textarea#whats-new").val();
-
-		jq.post(ajaxurl, {
-			action : 'add_activity_for_comment_post',
-			'content' : content,
-		}, function(response) {
-
-			//alert("response" + response);
+	/**
+	 * Student Registration
+	 */
+	jq("[href='#student_register']").click(function() {
+		
+		jq("#student_register_empty_area #student_register_modal").html(""); //Empty drawing area for student register modal
+		jq("#student_register_empty_area #student_register_modal").html(jq("#student_register_modal_wrapper").html()); //Get form
+		
+		jq("#student_register_empty_area #student_register_form").validate({
+				rules: {
+					name: {
+						required: true,
+						maxlength: 50
+					},
+					email: {
+						email: true,
+						maxlength: 100
+					},
+					phone: {
+						required: true,
+						maxlength: 10,
+						digits: true
+					},
+					level: {
+						required: true,
+						minlength: 1
+					},
+					student_number: {
+						required: true,
+						minlength: 1
+					},
+					detail: {
+						required: true,
+						maxlength: 500
+					},
+					location: {
+						required: true,
+						maxlength: 500
+					},
+					other: {
+						maxlength: 500
+					},
+				},
+				messages: {
+					name: {
+						required:"Please enter your name",
+					},
+					email: {
+						required:"Please enter a valid email address"
+					},
+					phone: {
+						required: "Please enter your phone number",
+						maxlength: "Your phone number must not be more than 10 digits"
+					},
+					level: {
+						required: "Please enter student level",
+					},
+					student_number: {
+						required: "Please enter the number of students",
+					},
+					detail: {
+						required: "Please enter detail",
+						maxlength: "Only 500 charactors are allowed to enter",
+					},
+					location: {
+						required: "Please enter location",
+						maxlength: "Only 500 charactors are allowed to enter",
+					},
+					other: {
+						maxlength: "Only 500 charactors are allowed to enter",
+					},
+				},
+				submitHandler: function(form) 
+				{
+					 jq.post(AonzAjax.ajaxurl, {
+							action : 'aonz_student_registration',
+							'name' : jq("#student_register_modal #student_register_form #cname").val(),
+							'email' : jq("#student_register_modal #student_register_form #cemail").val(),
+							'phone' : jq("#student_register_modal #student_register_form #cphone").val(),
+							'level' : jq("#student_register_modal #student_register_form #clevel").val(),
+							'student_number' : jq("#student_register_modal #student_register_form #cstudent_number").val(),
+							'detail' : jq("#student_register_modal #student_register_form #cdetail").val(),
+							'location' : jq("#student_register_modal #student_register_form #clocation").val(),
+							'other' : jq("#student_register_modal #student_register_form #cother").val(),
+						}, function(response) {
+				
+							jq("#student_register_modal #student_register_form").hide();
+							if(response=="success")
+							{
+								jq("#student_register_modal #request_success").show();
+							}
+							else
+							{
+								jq("#student_register_modal #request_fail").show();
+							}
+						});
+				 }
 		});
-		// alert("Aonz");
-		// return false; //This will cancel submit form
+		
+		jq('#student_register_empty_area #student_register_modal').modal({
+			overlayId : 'aonz-simplemodal-overlay',
+			containerId : 'aonz-simplemodal-container',
+			opacity : 85,
+			// onShow: SimpleModalLogin.show,
+			position : [ '15%', null ],
+			zIndex : 10000
+		});
 	});
 });
+
--- home/wp-content/plugins/aonz-webly-plugin/main.php
+++ home/wp-content/plugins/aonz-webly-plugin/main.php
@@ -60,32 +60,182 @@ class AonzWeblyPlugin
 
 		// add style
 		wp_enqueue_style('aonz-css', $this->plugin_url . "/css/aonz-default.css", false, $this->version, 'screen');
+// 		wp_enqueue_style('wizard-css', $this->plugin_url . "/css/wizard.css", false, $this->version, 'screen');
 		
 		// adding javascript files
-// 		wp_enqueue_script( 'jquery-simplemodal', $this->plugin_url . '/js/jquery.simplemodal.js', array( 'jquery' ) );
+		wp_enqueue_script( 'jquery-simplemodal', $this->plugin_url . '/js/jquery.simplemodal.js', array( 'jquery' ) );
+		wp_enqueue_script( 'jquery-validate-js', $this->plugin_url . '/js/jquery.validate.js', array( 'jquery' ) );
 		wp_enqueue_script( 'aonz-js', $this->plugin_url . '/js/aonz-js.js', array( 'jquery' ) );
 		
-		// test ajax
-		add_action('wp_ajax_aonz_ajax_test_callback', array($this, 'aonz_ajax_test_callback'));
+		// declare the URL to the file that handles the AJAX request (wp-admin/admin-ajax.php)
+		wp_localize_script( 'aonz-js', 'AonzAjax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
 		
-		// add facebook 
-		add_action('wp_footer', array($this, 'tutor_registor_modal'));
+		// Add Tutor Registration
+		add_action('wp_footer', array($this, 'tutor_register_modal'));
+		
+		// Add Student Registration
+		add_action('wp_footer', array($this, 'student_registration_empty_area'));
+		add_action('wp_footer', array($this, 'student_registration_form_modal'));
+		
+		// Student Registration Callback
+		add_action('wp_ajax_aonz_student_registration', array($this, 'aonz_student_registration_callback'));
+		add_action('wp_ajax_nopriv_aonz_student_registration', array($this, 'aonz_student_registration_callback'));
 	}
 	
 	/**
 	 * This function will exploit wp-fb-autoconnect plugin
 	 */
-	function tutor_registor_modal()
+	function tutor_register_modal()
 	{
 		// Write fb login function
-		jfb_output_facebook_instapopup();?>
-		
-			<div id="tutor_register_modal" style="display:none;">
+		jfb_output_facebook_instapopup();
+		?>
+			<div id="tutor_register_modal" style="display:none;width: 370px;">
 					<a href="#" onclick="showInstaPopup();return false;" class="aonz-fb-login-button"></a>
 			</div>
-			<?php 
-		}
+		<?php 
+	}
+	
+	/**
+	 * Empty area for adding student_register_modal later
+	 */
+	function student_registration_empty_area()
+	{
+	?>
+		<div id="student_register_empty_area">
+			<div id="student_register_modal" style="display:none;width: 700px;height: 500px">
+			<!-- Form will be inserted here -->
+			</div>
+		</div>
+	<?php 	
+	}
+	
+	/**
+	 * Student registration
+	 */
+	function student_registration_form_modal()
+	{
+		?>
+		<div id="student_register_modal_wrapper">
+			<form class="cmxform" id="student_register_form" method="post" action="" style="width: 700px;height: 500px;overflow-y: scroll;">
+			 <fieldset>
+			   <legend class="title">กรอกแบบฟอร์มเพื่อสมัครเรียน</legend>
+			   <p>
+			     <label for="cname">ชื่อ-นามสกุล ผู้ติดต่อ</label>
+			     <span class="required">*</span>
+			     <input id="cname" name="name" type="text" size="25" class="required" minlength="2" />
+			   </p>
+			   <p>
+			     <label for="cphone">เบอร์โทรศัพท์ที่ติดต่อได้</label>
+			     <span class="required">*</span>
+			     <span style="display:block;">กรอกตัวเลขติดกันทั้งหมด ตัวอย่าง 0821234567</span>
+			     <input id="cphone" name="phone" type="text" size="25"  class="phone" value="" />
+			   </p>
+			   <p>
+			     <label for="cemail">อีเมล์</label>
+			     <input id="cemail" name="email" size="25" type="text"  class="email" />
+			   </p>
+			   <p>
+			     <label for="clevel">ระดับชั้นผู้เรียน</label>
+			     <span class="required">*</span>
+			     <select id="clevel" name="level">
+					  <option value="1">อนุบาล</option>
+					  <option value="2">ประถมต้น</option>
+					  <option value="3" selected="selected">ประถมปลาย</option>
+					  <option value="4">มัธยมต้น</option>
+					  <option value="5">มัธยมปลาย</option>
+					  <option value="6">มหาวิทยาลัย</option>
+					  <option value="7">บุคคลทั่วไป</option>
+				 </select>
+			   </p>	  
+			   <p>
+			     <label for="cstudent_number">จำนวนผู้เรียน</label>
+			     <span class="required">*</span>
+		     	 <select id="cstudent_number" name="student_number">
+				  	<option value="1" selected="selected">1</option>
+				  	<option value="2">2</option>
+				  	<option value="3">3</option>
+				  	<option value="4">4</option>
+				  	<option value="5">5</option>
+				 </select>
+			   </p>
+			   <p>
+			     <label for="cdetail">รายละเอียดวิชาที่ต้องการเรียน</label>
+			     <span class="required">*</span>
+			     <span style="display:block;">ตัวอย่างการกรอก(ระบุหลายวิชาได้) : 
+			   	 	<br />วิชาคณิตศาตร์ - ป.5 - ทุกวันจันทร์ - เวลา 17.00 - 19.00 น.
+			   	 	<br />วิชาวิทยาศาสตร์ - ป.5 - ทุกวันพุธ - เวลา 17.00 - 19.00 น. 
+			   	 </span>
+			     <textarea id="cdetail" name="detail" cols="22"  class="required"></textarea>
+			   </p>
+			   <p>
+			     <label for="clocation">สถานที่เรียนที่สะดวก</label>
+			     <span class="required">*</span>
+			     <span style="display:block;">กรุณาระบุสถานที่ที่สะดวกสำหรับเรียน เช่น  บ้านเลขที่ ถนน ซอย จุดสังเกต(ถ้ามี) ให้ชัดเจน</span>	
+			     <textarea id="clocation" name="location" cols="22"  class="required"></textarea>
+			   </p>
+			   <p>
+			     <label for="cother">รายละเอียดอื่นๆ เช่น ระบุครูผู้สอน ฯลฯ</label>
+			     <textarea id="cother" name="other" cols="22"></textarea>
+			   </p>
+			   <p>
+			   	 <input class="submit" type="submit" value="Submit"/>
+			   	 <input type="button" class="simplemodal-close" value="Cancel"/>
+			 	</p>
+			 </fieldset>
+			 </form>
+			 
+			 <form id="request_success" style="display:none">
+				<p class="title" align="center">ระบบได้รับแบบฟอร์มสมัครเรียนของท่านเรียบร้อยแล้ว<br />
+				ทางทีมงานจะติดต่อกลับตามเบอร์โทรศัพท์ที่ท่านได้ระบุไว้อย่างเร็วที่สุด
+				</p>
+				<input type="button" class="simplemodal-close" value="Close"/>
+			</form>
+			<form id="request_fail" style="display:none">
+				<p class="title" align="center">
+				ข้อมูลที่กรอกไม่ถูกต้อง กรุณากรอกแบบฟอร์มใหม่อีกครั้ง<br />
+				ทางทีมงานขออภัยในความไม่สะดวก
+				</p>
+				<input type="button" class="simplemodal-close" value="Close"/>
+			</form>
+			
+		</div>
+		<?php 
+	}
+	
+	function aonz_student_registration_callback()
+	{
+		global $wpdb,$table_prefix;
 		
+		$name = esc_html($_POST['name']);
+		$email = esc_html($_POST['email']);
+		$phone = intval(esc_html($_POST['phone']));
+		$level = intval(esc_html($_POST['level']));
+		$student_number = intval(esc_html($_POST['student_number']));
+		$detail = esc_html($_POST['detail']);
+		$location = esc_html($_POST['location']);
+		$other = esc_html($_POST['other']);
+		
+		//Insert to db
+		$result = $wpdb->insert(
+				$table_prefix.'aonz_tutor_request',
+				array(
+						'name' => $name,
+						'email' => $email,
+						'phone' => $phone,
+						'level' => $level,
+						'student_number' => $student_number,
+						'detail' => $detail,
+						'location' => $location,
+						'other' => $other
+		));
+
+		if($result)
+			echo "success";
+		else
+			echo "fail";
+		die();
+	}
 }
 
 function aonz_init()
@@ -94,33 +244,5 @@ function aonz_init()
 	$aonz->init();
 }
 
-function aonz_ajax_test_callback()
-{
-	//*** Add comment post activity already exist functionality
-// 	global $post;
-// 	$content = $_POST['content'];
-// 	// Now write the values
-// 	// Record this on the user's profile
-// 	$user_id = bp_loggedin_user_id();
-// 	$from_user_link   = bp_core_get_userlink( $user_id );
-// 	$activity_action  = sprintf( __( '%s posted an update', 'buddypress' ), $from_user_link );
-// 	$activity_content = $content;
-// 	$primary_link     = bp_core_get_userlink( $user_id, false, true );
-
-// 	// Now write the values
-// 	$activity_id = bp_activity_add( array(
-// 		'user_id'      => $user_id,
-// 		'action'       => apply_filters( 'bp_activity_new_update_action', $activity_action ),
-// 		'content'      => apply_filters( 'bp_activity_new_update_content', $activity_content ),
-// 		'primary_link' => apply_filters( 'bp_activity_new_update_primary_link', $primary_link ),
-// 		'component'    => $bp->activity->id,
-// 		'type'         => 'activity_comment_post'
-// 	) );
-	
-// 	bp_activity_update_meta($activity_id, "post_id", $post->ID );
-}
-
-
-	
 add_action('after_setup_theme', 'aonz_init');
 ?>
